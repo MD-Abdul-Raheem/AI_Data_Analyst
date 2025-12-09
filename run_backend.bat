e325900d5de7ddfc85edfd6d7a711e23ee9169c4
MD Abdul Raheem <mdabdulraheem335@gmail.com>
2025-12-09 22:09:01 +0530

    Clean up: Remove duplicate and unnecessary files

--- run_backend.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd backend
-if not exist venv (
-    echo Creating virtual environment...
-    python -m venv venv
-)
-call venv\Scripts\activate
-pip install -r requirements.txt
-python -m backend.main
--- run_frontend.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd frontend
-if not exist node_modules (
-    echo Installing dependencies...
-    npm install
-)
-npm run dev
